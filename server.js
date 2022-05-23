824aa0a04b47c373153fbadfad35c74c3cf1b15a
Rahul Sharma <80841961+RahulSharma119@users.noreply.github.com>
2022-05-23 21:36:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,36 +10,15 @@ mongoose.connect('mongodb://localhost:27017/urlShortener',{
     useNewUrlParser : true , useUnifiedTopology : true
 })
 
-// app.set('view engine', 'ejs');
-// app.use(express.urlencoded({extended : false}))
 app.use(express.json());
 
-// app.get('/',async (req,res) => {
-//     const shortUrls = await ShortUrl.find()
-//     res.render('index.ejs',{urls : shortUrls});
-// });
 
 app.use(homeRoute);
 
 app.use(signinRoute);
 app.use(signupRoute);
 
-// app.get('/signin',async (req,res) => {
-//     res.render('signin.ejs');
-// });
-// app.post('/signin/check',async (req,res) => {
-//     console.log(req.body);
-//     return signin(req,res);
-// })
-
-// app.post('/shortenUrl',async (req,res) => {
-    
-//     await ShortUrl.create({full : req.body.fullurl});
-//     res.redirect('/');
-// });
-
 app.get('/:shortUrl',async (req,res)=>{
-    // console.log(req.params.shortUrl);
     const shortUrl = await ShortUrl.findOne({short : req.params.shortUrl});
     if(shortUrl == null){
         return res.sendStatus(404);
